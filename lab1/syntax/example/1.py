e1b2f93a3e78920f617805f92ff51f37dc921148
Ляззат Шамитова <lazzatsamitova@MacBook-Air-Lazzat.local>
2024-01-24 15:28:56 +0600

    syntax example

---
+++ lab1/syntax/example/1.py
@@ -0,0 +1,29 @@
+#example 1
+if 5 > 2:
+  print("Five is greater than two!")
+
+#example 2 (Syntax Error)
+if 5 > 2:
+print("Five is greater than two!")
+
+#example 3
+if 5 > 2:
+ print("Five is greater than two!")  
+if 5 > 2:
+        print("Five is greater than two!") 
+
+#example 4 (Syntax Error)
+if 5 > 2:
+ print("Five is greater than two!") 
+        print("Five is greater than two!")
+ 
+#example 5
+x = 5
+y = "Hello, World!"
+
+#example 6
+#This is a comment.
+print("Hello, World!")
+
+
+
