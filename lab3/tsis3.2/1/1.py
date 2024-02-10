6d3be9f88b9ee3ed65703e3d6eddd25b1b145041
laazzaat <157579596+laazzaat@users.noreply.github.com>
2024-02-10 08:42:05 +0600

    Add files via upload

---
+++ lab3/tsis3.2/1/1.py
@@ -0,0 +1,10 @@
+grams = int(input("Write how many grams you want to convert to ounces : "))
+def converting(grams):
+    print("You need - ", 28.3495231 * grams, "ounces")
+
+converting(grams)
+student
+name and grade
+оценка из 100 баллов
+4 студента
+вывести у кого больше 70++
